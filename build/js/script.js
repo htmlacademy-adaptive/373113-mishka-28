51090321a1a617c6686330df1c031e1bc7dacee8
Динар Ташбулатов <tdinarf@mail.ru>
2023-05-18 23:36:07 +0500

    8 задание на проверку

--- build/js/script.js
+++
@@ -1,1 +0,0 @@
-const menuBtn=document.querySelector(".navigation__toggle"),menuNav=document.querySelector(".navigation");menuBtn&&menuBtn.addEventListener("click",(e=>{e.preventDefault(),menuNav.classList.toggle("navigation--opened")})),document.querySelector(".no-js")&&document.querySelector(".no-js").classList.remove("no-js");
